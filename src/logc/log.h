07e1263dad325f9ae44f5a59ededa96864abf143
Davide Paro <dparo@outlook.it>
2024-12-28 23:09:43 +0100

    Removed CMakeLists.txt submodules dependencies

---
+++ src/logc/log.h
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2020 rxi
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT license. See `log.c` for details.
+ */
+
+#ifndef LOG_H
+#define LOG_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <time.h>
+
+#define LOG_VERSION "0.1.0"
+
+#ifndef RXI_LOGC_DEFAULT_LEVEL
+  # define RXI_LOGC_DEFAULT_LEVEL 1
+#endif
+
+
+#if defined __GNUC__
+#  define RXI_LOGC_PRINTF_ATTRIB(n, m) __attribute__((format(printf, n, m)))
+#else
+#  define RXI_LOGC_PRINTF_ATTRIB(n, m)
+#endif
+
+
+
+typedef struct {
+  va_list ap;
+  const char *fmt;
+  const char *file;
+  struct tm *time;
+  void *udata;
+  int line;
+  int level;
+} log_Event;
+
+typedef void (*log_LogFn)(log_Event *ev);
+typedef void (*log_LockFn)(bool lock, void *udata);
+
+enum {
+    LOG_DEBUG = -32,
+    LOG_TRACE = 0,
+    LOG_INFO = 32,
+    LOG_WARN = 64,
+    LOG_ERROR = 96,
+    LOG_FATAL = 128,
+};
+
+#define log_debug(...) do { if (LOG_DEBUG >= RXI_LOGC_DEFAULT_LEVEL) log_log(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__); } while(0)
+#define log_trace(...) do { if (LOG_TRACE >= RXI_LOGC_DEFAULT_LEVEL) log_log(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__); } while(0)
+#define log_info(...)  do { if (LOG_INFO >= RXI_LOGC_DEFAULT_LEVEL) log_log(LOG_INFO,  __FILE__, __LINE__, __VA_ARGS__); } while(0)
+#define log_warn(...)  do { if (LOG_WARN >= RXI_LOGC_DEFAULT_LEVEL) log_log(LOG_WARN,  __FILE__, __LINE__, __VA_ARGS__); } while(0)
+#define log_error(...) do { if (LOG_ERROR >= RXI_LOGC_DEFAULT_LEVEL) log_log(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__); } while(0)
+#define log_fatal(...) do { if (LOG_FATAL >= RXI_LOGC_DEFAULT_LEVEL) log_log(LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__); } while(0)
+
+const char* log_level_string(int level);
+void log_set_lock(log_LockFn fn, void *udata);
+void log_set_level(int level);
+void log_set_quiet(bool enable);
+int log_add_callback(log_LogFn fn, void *udata, int level);
+int log_add_fp(FILE *fp, int level);
+
+
+void log_log(int level, const char *file, int line, const char *fmt, ...) RXI_LOGC_PRINTF_ATTRIB(4, 5);
+
+#endif
--- tests/std.c
+++ tests/std.c
@@ -5,88 +5,87 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <assert.h>
+#include "test.h"
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unity.h>
 
 #include <string.h>
 #include "std.h"
 
-void setUp(void) { /* This is run before EACH TEST */
+static int setUp(void **_state) { /* This is run before EACH TEST */
 }
 
-void tearDown(void) {}
+static int tearDown(void **_state) {}
 
-static void test_ptr_is_aligned(void) {
-    TEST_ASSERT_TRUE(PTR_IS_ALIGNED(NULL, 1));
-    TEST_ASSERT_TRUE(PTR_IS_ALIGNED(NULL, 2));
-    TEST_ASSERT_TRUE(PTR_IS_ALIGNED(NULL, 3));
-    TEST_ASSERT_TRUE(PTR_IS_ALIGNED(NULL, 4));
+static void test_ptr_is_aligned(void **_state) {
+    assert_true(PTR_IS_ALIGNED(NULL, 1));
+    assert_true(PTR_IS_ALIGNED(NULL, 2));
+    assert_true(PTR_IS_ALIGNED(NULL, 3));
+    assert_true(PTR_IS_ALIGNED(NULL, 4));
 
-    TEST_ASSERT_TRUE(PTR_IS_ALIGNED(1, 1));
-    TEST_ASSERT_FALSE(PTR_IS_ALIGNED(1, 2));
+    assert_true(PTR_IS_ALIGNED(1, 1));
+    assert_false(PTR_IS_ALIGNED(1, 2));
 
-    TEST_ASSERT_TRUE(PTR_IS_ALIGNED(4, 1));
-    TEST_ASSERT_TRUE(PTR_IS_ALIGNED(4, 2));
-    TEST_ASSERT_FALSE(PTR_IS_ALIGNED(4, 3));
-    TEST_ASSERT_TRUE(PTR_IS_ALIGNED(4, 4));
+    assert_true(PTR_IS_ALIGNED(4, 1));
+    assert_true(PTR_IS_ALIGNED(4, 2));
+    assert_false(PTR_IS_ALIGNED(4, 3));
+    assert_true(PTR_IS_ALIGNED(4, 4));
 
-    TEST_ASSERT_TRUE(PTR_IS_ALIGNED(0, 128));
-    TEST_ASSERT_FALSE(PTR_IS_ALIGNED(1, 128));
-    TEST_ASSERT_FALSE(PTR_IS_ALIGNED(2, 128));
-    TEST_ASSERT_FALSE(PTR_IS_ALIGNED(4, 128));
-    TEST_ASSERT_FALSE(PTR_IS_ALIGNED(64, 128));
-    TEST_ASSERT_TRUE(PTR_IS_ALIGNED(128, 128));
-    TEST_ASSERT_TRUE(PTR_IS_ALIGNED(256, 128));
-    TEST_ASSERT_TRUE(PTR_IS_ALIGNED(512, 128));
+    assert_true(PTR_IS_ALIGNED(0, 128));
+    assert_false(PTR_IS_ALIGNED(1, 128));
+    assert_false(PTR_IS_ALIGNED(2, 128));
+    assert_false(PTR_IS_ALIGNED(4, 128));
+    assert_false(PTR_IS_ALIGNED(64, 128));
+    assert_true(PTR_IS_ALIGNED(128, 128));
+    assert_true(PTR_IS_ALIGNED(256, 128));
+    assert_true(PTR_IS_ALIGNED(512, 128));
 }
 
-static void test_ptr_align_up(void) {
-    TEST_ASSERT_EQUAL_PTR(0, PTR_ALIGN_UP(0, 512));
-    TEST_ASSERT_EQUAL_PTR(512, PTR_ALIGN_UP(1, 512));
-    TEST_ASSERT_EQUAL_PTR(512, PTR_ALIGN_UP(2, 512));
-    TEST_ASSERT_EQUAL_PTR(512, PTR_ALIGN_UP(511, 512));
-    TEST_ASSERT_EQUAL_PTR(512, PTR_ALIGN_UP(512, 512));
-    TEST_ASSERT_EQUAL_PTR(1024, PTR_ALIGN_UP(513, 512));
+static void test_ptr_align_up(void **_state) {
+    assert_ptr_equal(0, PTR_ALIGN_UP(0, 512));
+    assert_ptr_equal(512, PTR_ALIGN_UP(1, 512));
+    assert_ptr_equal(512, PTR_ALIGN_UP(2, 512));
+    assert_ptr_equal(512, PTR_ALIGN_UP(511, 512));
+    assert_ptr_equal(512, PTR_ALIGN_UP(512, 512));
+    assert_ptr_equal(1024, PTR_ALIGN_UP(513, 512));
 
-    TEST_ASSERT_EQUAL_PTR(0, PTR_ALIGN_UP(0, 3));
-    TEST_ASSERT_EQUAL_PTR(3, PTR_ALIGN_UP(1, 3));
-    TEST_ASSERT_EQUAL_PTR(3, PTR_ALIGN_UP(3, 3));
-    TEST_ASSERT_EQUAL_PTR(6, PTR_ALIGN_UP(4, 3));
+    assert_ptr_equal(0, PTR_ALIGN_UP(0, 3));
+    assert_ptr_equal(3, PTR_ALIGN_UP(1, 3));
+    assert_ptr_equal(3, PTR_ALIGN_UP(3, 3));
+    assert_ptr_equal(6, PTR_ALIGN_UP(4, 3));
 
-    TEST_ASSERT_EQUAL_PTR(0, PTR_ALIGN_UP_T((uint64_t *)0));
-    TEST_ASSERT_EQUAL_PTR(8, PTR_ALIGN_UP_T((uint64_t *)1));
-    TEST_ASSERT_EQUAL_PTR(8, PTR_ALIGN_UP_T((uint64_t *)2));
-    TEST_ASSERT_EQUAL_PTR(8, PTR_ALIGN_UP_T((uint64_t *)4));
-    TEST_ASSERT_EQUAL_PTR(8, PTR_ALIGN_UP_T((uint64_t *)7));
-    TEST_ASSERT_EQUAL_PTR(8, PTR_ALIGN_UP_T((uint64_t *)8));
-    TEST_ASSERT_EQUAL_PTR(16, PTR_ALIGN_UP_T((uint64_t *)9));
+    assert_ptr_equal(0, PTR_ALIGN_UP_T((uint64_t *)0));
+    assert_ptr_equal(8, PTR_ALIGN_UP_T((uint64_t *)1));
+    assert_ptr_equal(8, PTR_ALIGN_UP_T((uint64_t *)2));
+    assert_ptr_equal(8, PTR_ALIGN_UP_T((uint64_t *)4));
+    assert_ptr_equal(8, PTR_ALIGN_UP_T((uint64_t *)7));
+    assert_ptr_equal(8, PTR_ALIGN_UP_T((uint64_t *)8));
+    assert_ptr_equal(16, PTR_ALIGN_UP_T((uint64_t *)9));
 }
 
-static void test_ptr_align_down(void) {
-    TEST_ASSERT_EQUAL_PTR(0, PTR_ALIGN_DOWN(0, 512));
-    TEST_ASSERT_EQUAL_PTR(0, PTR_ALIGN_DOWN(1, 512));
-    TEST_ASSERT_EQUAL_PTR(0, PTR_ALIGN_DOWN(2, 512));
-    TEST_ASSERT_EQUAL_PTR(0, PTR_ALIGN_DOWN(511, 512));
-    TEST_ASSERT_EQUAL_PTR(512, PTR_ALIGN_DOWN(512, 512));
-    TEST_ASSERT_EQUAL_PTR(512, PTR_ALIGN_DOWN(513, 512));
+static void test_ptr_align_down(void **_state) {
+    assert_ptr_equal(0, PTR_ALIGN_DOWN(0, 512));
+    assert_ptr_equal(0, PTR_ALIGN_DOWN(1, 512));
+    assert_ptr_equal(0, PTR_ALIGN_DOWN(2, 512));
+    assert_ptr_equal(0, PTR_ALIGN_DOWN(511, 512));
+    assert_ptr_equal(512, PTR_ALIGN_DOWN(512, 512));
+    assert_ptr_equal(512, PTR_ALIGN_DOWN(513, 512));
 
-    TEST_ASSERT_EQUAL_PTR(0, PTR_ALIGN_DOWN(0, 3));
-    TEST_ASSERT_EQUAL_PTR(0, PTR_ALIGN_DOWN(1, 3));
-    TEST_ASSERT_EQUAL_PTR(3, PTR_ALIGN_DOWN(3, 3));
-    TEST_ASSERT_EQUAL_PTR(3, PTR_ALIGN_DOWN(4, 3));
+    assert_ptr_equal(0, PTR_ALIGN_DOWN(0, 3));
+    assert_ptr_equal(0, PTR_ALIGN_DOWN(1, 3));
+    assert_ptr_equal(3, PTR_ALIGN_DOWN(3, 3));
+    assert_ptr_equal(3, PTR_ALIGN_DOWN(4, 3));
 
-    TEST_ASSERT_EQUAL_PTR(0, PTR_ALIGN_DOWN_T((uint64_t *)0));
-    TEST_ASSERT_EQUAL_PTR(0, PTR_ALIGN_DOWN_T((uint64_t *)1));
-    TEST_ASSERT_EQUAL_PTR(0, PTR_ALIGN_DOWN_T((uint64_t *)2));
-    TEST_ASSERT_EQUAL_PTR(0, PTR_ALIGN_DOWN_T((uint64_t *)4));
-    TEST_ASSERT_EQUAL_PTR(0, PTR_ALIGN_DOWN_T((uint64_t *)7));
-    TEST_ASSERT_EQUAL_PTR(8, PTR_ALIGN_DOWN_T((uint64_t *)8));
-    TEST_ASSERT_EQUAL_PTR(8, PTR_ALIGN_DOWN_T((uint64_t *)9));
+    assert_ptr_equal(0, PTR_ALIGN_DOWN_T((uint64_t *)0));
+    assert_ptr_equal(0, PTR_ALIGN_DOWN_T((uint64_t *)1));
+    assert_ptr_equal(0, PTR_ALIGN_DOWN_T((uint64_t *)2));
+    assert_ptr_equal(0, PTR_ALIGN_DOWN_T((uint64_t *)4));
+    assert_ptr_equal(0, PTR_ALIGN_DOWN_T((uint64_t *)7));
+    assert_ptr_equal(8, PTR_ALIGN_DOWN_T((uint64_t *)8));
+    assert_ptr_equal(8, PTR_ALIGN_DOWN_T((uint64_t *)9));
 }
 
 static void test_snprintf() {
@@ -94,26 +93,29 @@ static void test_snprintf() {
 
     memset(buf, 0, sizeof(buf));
     snprintf(buf, sizeof(buf), "%s", "A big payload. Much larger than the receiving buffer can handle");
-    TEST_ASSERT_EQUAL(0, buf[sizeof(buf) - 1]);
+    assert_int_equal(0, buf[sizeof(buf) - 1]);
 
     if (0)
     {
         memset(buf, 0, sizeof(buf));
         strncpy(buf, "A big payload. Much larger than the receiving buffer can handle", sizeof(buf));
-        TEST_ASSERT_EQUAL(0, buf[sizeof(buf) - 1]);
+        assert_int_equal(0, buf[sizeof(buf) - 1]);
     }
 
     char buf0[0] = "";
     char *result = strncpy(buf0, "foobar", 1);
-    TEST_ASSERT_EQUAL(buf0, result);
-    TEST_ASSERT_EQUAL(0, sizeof(buf0));
+    assert_int_equal(buf0, result);
+    assert_int_equal(0, sizeof(buf0));
 }
 
 int main(void) {
-    UNITY_BEGIN();
-    RUN_TEST(test_ptr_is_aligned);
-    RUN_TEST(test_ptr_align_up);
-    RUN_TEST(test_ptr_align_down);
-    RUN_TEST(test_snprintf);
-    return UNITY_END();
+    const struct CMUnitTest html5_render_escaped[] = {
+        // clang-format off
+        cmocka_unit_test(test_ptr_is_aligned),
+        cmocka_unit_test(test_ptr_align_up),
+        cmocka_unit_test(test_ptr_align_down),
+        cmocka_unit_test(test_snprintf),
+        // clang-format on
+    };
+    return cmocka_run_group_tests(html5_render_escaped, NULL, NULL);
 }
